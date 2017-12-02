59bd251389517f44fd8b4615f5fe3ef174ed7a0a
kirantembere <kirantembere@gmail.com>
2017-12-02 16:45:22 -0500

    serve html page with express server

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+var httpPort = 9000;
+var app = express();
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname,'/client/index.html'))
+})
+
+app.listen(httpPort,function(){
+  console.log('started')
+});
